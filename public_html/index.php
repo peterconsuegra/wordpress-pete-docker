1a59033bc0e9644e9ecc2d021396a0d53a5b347e
Pedro Consuegra <pedroconsuegrat@gmail.com>
2021-04-14 19:17:35 -0500

    redirec page to pete

---
+++ public_html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Redirectin to WordPress Pete</title>
+</head>
+<body>
+
+<h1>Redirecting..</h1>
+sss
+<script>
+	window.location.replace("http://pete.petelocal.net");
+</script>
+
+</body>
+</html>
